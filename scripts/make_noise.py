11c19f2ab4457a0ff30618a826d75def2f604de6
ViggieSmalls <victormovileanu@gmail.com>
2018-06-29 13:26:40 +0200

    added file

---
+++ scripts/make_noise.py
@@ -0,0 +1,23 @@
+import numpy as np
+import mrcfile
+
+def main(args):
+    output = "flat_noise.mrc"
+
+    ary = np.ones((1,args.shape[1],args.shape[0])) * args.x
+    ary = np.pad(ary, 1, 'constant', constant_values=0)
+
+    with mrcfile.new(output, overwrite=True) as new_mrc:
+        new_mrc.set_data(ary.astype(np.float32))
+        new_mrc.flush()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Create an mrc file with a constant value specified by the provided argument and padded with zeros.')
+    parser.add_argument('x', type=int, help='Noise level')
+    parser.add_argument('--shape', type=int, nargs=2, default=(5200,5200),  #FIXME check the default range for relion
+                        help='Shape of the output mrc file (no borders). (default = 5200 x 5200)')
+    args = parser.parse_args()
+    main(args)
+
